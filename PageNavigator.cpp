7432e157ce95e67ae56f9e1a1c3f05f337daec48
Marc Roßbach <mr@mail.com>
2021-01-24 22:01:43 +0100

    feat: add empty page support

--- PageNavigator.cpp
+++ PageNavigator.cpp
@@ -25,7 +25,10 @@ void PageNavigator::Click()
     if (_currentPageIdx < _pages.size())
     {
         auto& page = _pages[_currentPageIdx];
-        page.control->Click();
+        if (page.control != nullptr)
+        {
+            page.control->Click();
+        }
     }
 }
 
@@ -34,32 +37,53 @@ void PageNavigator::Scroll(const int delta)
     if (_currentPageIdx < _pages.size())
     {
         auto& page = _pages[_currentPageIdx];
-        if (page.control->IsSelected())
+        if (page.control != nullptr && page.control->IsSelected())
         {
             page.control->Scroll(delta);
             return;
         }
     }
-    _currentPageIdx = MathUtils::Modulo<int>(_currentPageIdx + delta, _pages.size());
+    _currentPageIdx = std::min<int>(_pages.size() - 1, std::max<int>(0, _currentPageIdx + delta));
 }
 
 void PageNavigator::Render(Paint& paint, const int x, const int y)
 {
     if (!_pages.empty())
     {
-        const int boxWidth = GetActualWidth();
+        const int boxWidth = GetActualWidth() - 1;
         const int boxHeight = GetActualHeight();
+        const auto& page = _pages[_currentPageIdx];
 
-        paint.DrawFilledRectangle(x, y, x + boxWidth, y + boxHeight, WHITE);
-        paint.DrawRectangle(x, y, x + boxWidth, y + boxHeight, BLACK);
+        int titleHeight = 0;
+        if (!page.title.empty())
+        {
+            titleHeight = _font.Height + 1;
 
-        auto& page = _pages[_currentPageIdx];
-        paint.DrawUtf8StringAt(x + boxWidth / 2, y + 1, page.title.c_str(), &_font, BLACK, TextAlignment::CENTER);
+            paint.DrawFilledRectangle(x, y, x + boxWidth, y + _font.Height + 1, WHITE);
+            paint.DrawRectangle(x, y, x + boxWidth, y + _font.Height + 1, BLACK);
+            
+            const int yPos = y + 1;
+            paint.DrawUtf8StringAt(x + boxWidth / 2, yPos, page.title.c_str(), &_font, BLACK, TextAlignment::CENTER);
 
-        paint.DrawHorizontalLine(x, y + _font.Height + 1, boxWidth, BLACK);
+            if (_currentPageIdx > 0)
+            {
+                paint.DrawUtf8StringAt(x + 3, yPos, "<", &_font, BLACK, TextAlignment::LEFT);
+            }
 
-        page.control->SetWdith(boxWidth - 4);
-        page.control->SetHeight(boxHeight - _font.Height - 3);
-        page.control->Render(paint, x + 2, y + _font.Height + 3);
+            if (_currentPageIdx + 1 < _pages.size())
+            {
+                paint.DrawUtf8StringAt(x + boxWidth - 1, yPos, ">", &_font, BLACK, TextAlignment::RIGHT);
+            }
+        }
+
+        if (page.control != nullptr)
+        {
+            paint.DrawFilledRectangle(x, y + titleHeight, x + boxWidth, y + boxHeight, WHITE);
+            paint.DrawRectangle(x, y + titleHeight, x + boxWidth, y + boxHeight, BLACK);
+
+            page.control->SetWdith(boxWidth - 4);
+            page.control->SetHeight(boxHeight - titleHeight - 3);
+            page.control->Render(paint, x + 2, y + titleHeight + 2);
+        }
     }
 }
